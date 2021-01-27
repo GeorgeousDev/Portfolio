2e8fe6a5c9cb3f2d90c39384e877460169a15d33
Jerzy Pędlowski <jerzy.pedlowski@gmail.com>
2021-01-27 14:59:25 +0100

    Updates

---
+++ precache-manifest.56c2e00973e7d8b87a93a64e7ed1742a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafcad30957261d1141f0e794a8deb8e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d392b5d459bcb2aa600d",
+    "url": "/Portfolio/static/js/2.e6ae4a69.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/Portfolio/static/js/2.e6ae4a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1cf61b88b4a8ebac4d",
+    "url": "/Portfolio/static/js/main.796db728.chunk.js"
+  },
+  {
+    "revision": "a55af25d7fc0e530f39f",
+    "url": "/Portfolio/static/js/runtime-main.e94b03ce.js"
+  },
+  {
+    "revision": "07e01133e1f5daf0886eafbee81f53dc",
+    "url": "/Portfolio/static/media/arrowRight.07e01133.svg"
+  },
+  {
+    "revision": "7ae4b13bc7b6758923dd5b2373559a53",
+    "url": "/Portfolio/static/media/css.7ae4b13b.svg"
+  },
+  {
+    "revision": "c5bdc7a6506d0b4ed1e5891e60635619",
+    "url": "/Portfolio/static/media/gameApp1.c5bdc7a6.png"
+  },
+  {
+    "revision": "1d9c5c96a414be25ac2eb0d41c0d4ddd",
+    "url": "/Portfolio/static/media/gameApp2.1d9c5c96.png"
+  },
+  {
+    "revision": "b8400824259b568ed1cc74774d8c3368",
+    "url": "/Portfolio/static/media/gameApp3.b8400824.png"
+  },
+  {
+    "revision": "bf016c7402b0d3de950114f84fc4886c",
+    "url": "/Portfolio/static/media/github.bf016c74.svg"
+  },
+  {
+    "revision": "deda33aea2bfa9216df4b9abc2d17698",
+    "url": "/Portfolio/static/media/gmail.deda33ae.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/Portfolio/static/media/html5.24845c5c.svg"
+  },
+  {
+    "revision": "dc2fc5be0984077b3b0fce8c0b8f045a",
+    "url": "/Portfolio/static/media/javascript.dc2fc5be.svg"
+  },
+  {
+    "revision": "0cccf34f12cf56872787db8872675487",
+    "url": "/Portfolio/static/media/linkedin.0cccf34f.svg"
+  },
+  {
+    "revision": "baa5c7dc66e706261c9d25bd3e4d5d65",
+    "url": "/Portfolio/static/media/musicApp1.baa5c7dc.png"
+  },
+  {
+    "revision": "735bb89adc143f7e0284b97d82af62f8",
+    "url": "/Portfolio/static/media/musicApp2.735bb89a.png"
+  },
+  {
+    "revision": "7efa2eb7357ab0da243ff549926729b2",
+    "url": "/Portfolio/static/media/musicApp3.7efa2eb7.png"
+  },
+  {
+    "revision": "2f09304245f72b9d15521554adb753a1",
+    "url": "/Portfolio/static/media/profil.2f093042.jpg"
+  },
+  {
+    "revision": "dd4048bbd34a88865d4153d3ce3bbf81",
+    "url": "/Portfolio/static/media/programming3.dd4048bb.jpg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/Portfolio/static/media/react.9a28da9f.svg"
+  }
+]);
